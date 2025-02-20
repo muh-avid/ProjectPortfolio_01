0095d086a463d2d1459f53b36be562420cf96da7
muh-avid <avidm8434@gmail.com>
2025-02-20 21:18:08 +0800

    editFIle

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const toggleBtn = document.querySelector('.toggle_btn');
 const toggleBtnIcon = document.querySelector('.toggle_btn i');
 const dropdownMenu = document.querySelector('.dropdown_menu');
+const text = document.getElementById("text");
+const button = document.getElementById("toggleButton");
 
 toggleBtn.addEventListener('click', function(){
     dropdownMenu.classList.toggle('open');
@@ -8,3 +10,16 @@ toggleBtn.addEventListener('click', function(){
 
     toggleBtnIcon.className = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
 });
+
+
+
+        
+        button.addEventListener("click", () => {
+            if (text.classList.contains("line-clamp-3")) {
+                text.classList.remove("line-clamp-3");
+                button.textContent = "Read Less";
+            } else {
+                text.classList.add("line-clamp-3");
+                button.textContent = "Read More";
+            }
+        });
